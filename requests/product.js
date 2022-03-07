90b62eb222d552988261978a5cc06eff43f18143
Volkan Ulker <vlkn-ulkr@hotmail.com>
2022-03-08 00:05:29 +0300

    Views are modified.

---
+++ requests/product.js
@@ -0,0 +1,45 @@
+const request = require('request')
+
+const urlBase = "https://osf-digital-backend-academy.herokuapp.com/api/products/product_search"
+
+
+const secretKey ="$2a$08$snapaHu1X69uonct1IluteSCG4e1QJAUmtWSmEoX8lroGixq5/UqS"
+
+
+const errMessage = 'Unable to connect to the Backend Service!'
+
+
+const getProductById = (id, callback) => {
+    const url =`${urlBase}?id=${id}&secretKey=${secretKey}`
+
+    request({url:url, json:true}, (error, response) => {
+        if(error){
+            callback(errMessage, undefined)
+        } else{
+            const data = response.body
+            callback(undefined, data)
+        }
+    }) 
+}
+
+
+
+const getProductByCategoryId = (id, callback) => {
+    const url =`${urlBase}?primary_category_id=${id}&secretKey=${secretKey}`
+
+    request({url:url, json:true}, (error, response) => {
+        if(error){
+            callback(errMessage, undefined)
+        } else{
+            const data = response.body
+            callback(undefined, data)
+        }
+    }) 
+}
+
+
+
+module.exports = {
+    getProductById:getProductById,
+    getProductByCategoryId:getProductByCategoryId
+}
