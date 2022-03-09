e45ab4c1db244991c2998bb20deaa07d0a36eee6
Volkan Ulker <vlkn-ulkr@hotmail.com>
2022-03-09 22:00:55 +0300

    Unnecessary code block is deleted from index route.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var categoryRequest = require('../requests/category/category')
+
 /* GET home page. */
 router.get('/',(req, res, next) => {
-
-  categoryRequest.getAllParentCategories('mens', (error, data) => {
-    if(!error){
       res.render('index');
-    }else {
-      res.render('error', {message:'An error occured.'})
-    }
-  })
+})
 
   
-});
 
 module.exports = router;
