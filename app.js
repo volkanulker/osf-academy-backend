1e4de266fba075a851133e246c57480ef08d5d4a
Volkan Ulker <vlkn-ulkr@hotmail.com>
2022-03-10 18:51:19 +0300

    Dynamic product cards are added.

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 
-app.use('/home', categoryRouter)
+app.use('/category', categoryRouter)
 
 app.use('/product',productRouter)
 
--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const productRequest = require("../requests/product");
 
+router.get("/:subCategory", function (req, res, next) {
+  const subCategory = req.params.subCategory;
 
-router.get('/', function(req, res, next) {
-  res.render('./product/productCard');
+  productRequest.getProductByCategoryId(subCategory, (error, data) => {
+    if (error) {
+      return console.log(error);
+    } else if (data.error) {
+      return res.render('error')
+    } else {
+      let halfwayThrough = Math.ceil(data.length / 2);
+      const productsOnLeft = data.slice(0, halfwayThrough);
+      const productsOnRight = data.slice(halfwayThrough, data.length);
+
+      res.render("./product/productCard", {
+        productsOnLeft: productsOnLeft,
+        productsOnRight: productsOnRight,
+      });
+    }
+  });
 });
 
-router.get('/product-detail', function(req, res, next) {
-  res.render('product/productDetail')
-} )
-
-
+router.get("/product-detail", function (req, res, next) {
+  res.render("product/productDetail");
+});
 
 module.exports = router;
